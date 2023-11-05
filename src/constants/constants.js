14d4f7cc9c0f5b125df45f5d58cef581281b45c0
sakshammishra5 <sakshammishra5thsep@gmail.com>
2023-11-05 22:53:07 +0530

    token changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const GITHUB_TOKEN="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"
+export const GITHUB_TOKEN="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"
 export const GET_USER_DETAILS="https://api.github.com/users/"
 
 export const GET_USER_REPO=`https://api.github.com/users/`
